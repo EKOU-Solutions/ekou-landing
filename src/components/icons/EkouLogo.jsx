e061fab086bc7b395d34146402b2e1a26e5b2649
Jouler Talaigua <joulert02@gmail.com>
2026-02-28 19:57:02 -0500

    Feature/assets update (#43)
    * feat: update team images and enhance styling for TeamMemberCard and Welcome components
    
    * feat: add CollaboratorCard component for displaying team members with social links
    
    * feat: add TypeScript type definitions for EkouLogo component props
    
    * feat: update layout for collaborators section to use a responsive grid

--- src/components/icons/EkouLogo.jsx
+++ src/components/icons/EkouLogo.jsx
@@ -1,5 +1,14 @@
 import ekouLogo from '../../assets/icons/ekouLogo-v1.svg'
 
+/**
+ * @typedef {Object} EkouLogoProps
+ * @property {string} [className] - CSS class names
+ * @property {string} [alt] - Alt text for the logo image
+ * @property {string} [btnAria] - Aria label for the link
+ * @property {boolean} [showText] - Whether to show "EKOU" text
+ */
+
+/** @type {React.FC<EkouLogoProps>} */
 const EkouLogo = ({ className, alt, btnAria, showText = true } = {}) => {
   const image = <img src={ekouLogo.src} alt={alt} className={className} />
 
